2b6306849912f6b4f29c5ef4cd4a2765dec09440
Crowdbotics <team@crowdbotics.com>
2021-09-13 02:20:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "orange_river_30491Identifier",
+  url: "https://orange_river_30491Identifier.botics.co",
+  api: "https://orange_river_30491Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
